911f8a46583f6869d13d01367b8f1b560b45925b
diegofsousa <diegofernando5672@gmail.com>
2017-04-07 21:48:16 -0300

    first commit

---
+++ api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.contrib.auth.models import User
+
+class StatusView(APIView):
+
+	def get(self, request, format=None):
+		try:
+			user = User.objects.filter(is_superuser=True)
+			return Response('Servidor: ' +str(user.first()) + ' - Status: OK!')
+		except Exception as e:
+			user = User.objects.filter(is_superuser=True)
+			return Response('Servidor: ' +str(user.first()) + ' - Status: instavel :/')
+		
+
+	def post(self, request, format=None):
+		try:
+			listaIter = []
+			listaStr = request.data.split(',')
+			for i in listaStr:listaIter.append(int(i))
+			listaIter.sort()
+			print(listaIter)
+			return Response(listaIter, status=status.HTTP_201_CREATED)
+		except Exception as e:
+			return Response({"message":"403 Forbidden"}, status=status.HTTP_409_CONFLICT)
