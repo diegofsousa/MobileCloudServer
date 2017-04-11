5e7488c29bb488db268fb03f2ccab8071e075e05
diegofsousa <diegofernando5672@gmail.com>
2017-04-11 16:41:19 -0300

    add ip detection and makefile

--- CloudServerAPI/wsgi.py
+++ CloudServerAPI/wsgi.py
@@ -8,8 +8,24 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import netifaces
 
 from django.core.wsgi import get_wsgi_application
+
+try:
+	print('IP local wifi: ' + netifaces.ifaddresses('wlan0')[2][0]['addr'])
+except Exception as e:
+	print('Interface Wifi não detectada')
+
+try:
+	print('IP local cabo: ' + netifaces.ifaddresses('eth0')[2][0]['addr'])
+except Exception as e:
+	print('Interface cabeada não detectada')
+
+try:
+	print('IP VM: ' + netifaces.ifaddresses('enp0s3')[2][0]['addr'])
+except Exception as e:
+	print('Interface cabeada não detectada')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CloudServerAPI.settings")
 
